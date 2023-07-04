0b298d36290159839fbde5068e35f1c01e78ffd5
KenStarry <starrycodes@gmail.com>
2023-07-04 19:55:51 +0300

    Finished Animated padding

--- lib/widgets/animation/animated_padding.dart
+++ lib/widgets/animation/animated_padding.dart
@@ -8,7 +8,6 @@ class MyAnimatedPadding extends StatefulWidget {
 }
 
 class _MyAnimatedPaddingState extends State<MyAnimatedPadding> {
-
   double padValue = 0.0;
 
   @override
@@ -16,11 +15,23 @@ class _MyAnimatedPaddingState extends State<MyAnimatedPadding> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ElevatedButton(onPressed: (){
-          setState(() {
-            padValue = padValue == 0 ? 100.0 : 0.0;
-          });
-        }, child: const Text("Change Padding"))
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                padValue = padValue == 0 ? 100.0 : 0.0;
+              });
+            },
+            child: const Text("Change Padding")),
+        AnimatedPadding(
+          padding: EdgeInsets.all(padValue),
+          duration: const Duration(seconds: 2),
+          curve: Curves.easeIn,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 4,
+            color: Colors.redAccent,
+          ),
+        )
       ],
     );
   }
