4366fc4f650700a25577bda9ba5fe5fc59549392
KenStarry <starrycodes@gmail.com>
2023-07-04 20:39:48 +0300

    Animated Rotations widget

---
+++ lib/widgets/animation/animated_rotation.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedRotation extends StatefulWidget {
+  const MyAnimatedRotation({super.key});
+
+  @override
+  State<MyAnimatedRotation> createState() => _MyAnimatedRotationState();
+}
+
+class _MyAnimatedRotationState extends State<MyAnimatedRotation> {
+
+  double turns = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
