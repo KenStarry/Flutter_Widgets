d190039f7fc50ecaf221eed153bf208768994582
KenStarry <starrycodes@gmail.com>
2023-07-04 20:59:30 +0300

    Animated Size Widget

---
+++ lib/widgets/animation/animated_size.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedSize extends StatefulWidget {
+  const MyAnimatedSize({super.key});
+
+  @override
+  State<MyAnimatedSize> createState() => _MyAnimatedSizeState();
+}
+
+class _MyAnimatedSizeState extends State<MyAnimatedSize> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
