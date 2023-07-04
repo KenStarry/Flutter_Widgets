0e02e6a367b6ee500da0aeb7b3afb08509261e52
KenStarry <starrycodes@gmail.com>
2023-07-04 20:17:39 +0300

    Animated Positioned widget

---
+++ lib/widgets/animation/animated_positioned.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedPositioned extends StatefulWidget {
+  const MyAnimatedPositioned({super.key});
+
+  @override
+  State<MyAnimatedPositioned> createState() => _MyAnimatedPositionedState();
+}
+
+class _MyAnimatedPositionedState extends State<MyAnimatedPositioned> {
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
