0e81849084378f35373cb189557c89522a91be49
KenStarry <starrycodes@gmail.com>
2023-07-04 20:00:45 +0300

    Animated Physical model

---
+++ lib/widgets/animation/animated_physical_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedPhysicalModel extends StatefulWidget {
+  const MyAnimatedPhysicalModel({super.key});
+
+  @override
+  State<MyAnimatedPhysicalModel> createState() => _MyAnimatedPhysicalModelState();
+}
+
+class _MyAnimatedPhysicalModelState extends State<MyAnimatedPhysicalModel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
