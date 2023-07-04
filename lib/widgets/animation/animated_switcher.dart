09c9fe65bb7b00a0b32968b303f06807d112fb0b
KenStarry <starrycodes@gmail.com>
2023-07-04 22:09:24 +0300

    Added Transition builder

--- lib/widgets/animation/animated_switcher.dart
+++ lib/widgets/animation/animated_switcher.dart
@@ -20,6 +20,9 @@ class _MyAnimatedSwitcherState extends State<MyAnimatedSwitcher> {
           duration: Duration(milliseconds: 500),
           child: Text('$count',
           key: ValueKey(count),),
+          transitionBuilder: (child, animation){
+            return ScaleTransition(scale: animation, child: child,)
+          },
         ),
         ElevatedButton(onPressed: (){
           setState(() {
