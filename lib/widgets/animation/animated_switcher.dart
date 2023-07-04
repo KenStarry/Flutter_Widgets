ff74da66beeaf65d0fed1490813990d31d9bc93f
KenStarry <starrycodes@gmail.com>
2023-07-04 21:07:20 +0300

    Animated Switcher

---
+++ lib/widgets/animation/animated_switcher.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedSwitcher extends StatefulWidget {
+  const MyAnimatedSwitcher({super.key});
+
+  @override
+  State<MyAnimatedSwitcher> createState() => _MyAnimatedSwitcherState();
+}
+
+class _MyAnimatedSwitcherState extends State<MyAnimatedSwitcher> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
