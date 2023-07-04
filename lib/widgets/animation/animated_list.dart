d4d6d7d90999033901b30e9c23f646b75b9788ae
KenStarry <starrycodes@gmail.com>
2023-07-04 22:26:35 +0300

    Animated List

---
+++ lib/widgets/animation/animated_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAnimatedList extends StatefulWidget {
+  const MyAnimatedList({super.key});
+
+  @override
+  State<MyAnimatedList> createState() => _MyAnimatedListState();
+}
+
+class _MyAnimatedListState extends State<MyAnimatedList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
