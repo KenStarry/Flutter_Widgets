4ebaf3f063552b082dd3db6f17f2c1c7a8182bf1
KenStarry <starrycodes@gmail.com>
2023-07-04 22:12:24 +0300

    Animated Icon Widget and added mixin

--- lib/widgets/animation/animated_icon.dart
+++ lib/widgets/animation/animated_icon.dart
@@ -7,7 +7,7 @@ class MyAnimatedIcon extends StatefulWidget {
   State<MyAnimatedIcon> createState() => _MyAnimatedIconState();
 }
 
-class _MyAnimatedIconState extends State<MyAnimatedIcon> {
+class _MyAnimatedIconState extends State<MyAnimatedIcon> with TickerProviderStateMixin{
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
