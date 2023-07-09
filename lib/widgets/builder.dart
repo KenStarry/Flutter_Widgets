564b9ab611bf4be6c156165c7750c53e9643d369
KenStarry <starrycodes@gmail.com>
2023-07-09 15:24:52 +0300

    Builder widget

---
+++ lib/widgets/builder.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyBuilder extends StatelessWidget {
+  const MyBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
