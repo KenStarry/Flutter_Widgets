dadca84e1f14d124a3e16e06acab0a9bc81b6e40
KenStarry <starrycodes@gmail.com>
2023-07-09 20:16:56 +0300

    Draggable widget

---
+++ lib/widgets/draggable.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyDraggable extends StatefulWidget {
+  const MyDraggable({super.key});
+
+  @override
+  State<MyDraggable> createState() => _MyDraggableState();
+}
+
+class _MyDraggableState extends State<MyDraggable> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
