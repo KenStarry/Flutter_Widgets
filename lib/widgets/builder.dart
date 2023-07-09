8394edd234211b27053f93ffcdcd2a7fd4805169
KenStarry <starrycodes@gmail.com>
2023-07-09 15:27:12 +0300

    Builder widget finished for context

--- lib/widgets/builder.dart
+++ lib/widgets/builder.dart
@@ -5,6 +5,17 @@ class MyBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: myWidget(),
+    );
   }
 }
+
+//  Builder gives us the context
+
+myWidget() => Builder(builder: (context) {
+      return Text(
+        "Hello",
+        style: Theme.of(context).textTheme.displayLarge,
+      );
+    });
