19342e965d935dde163b98a8d400d0efd8f671c3
KenStarry <starrycodes@gmail.com>
2023-07-09 19:47:54 +0300

    Decorated box transition

---
+++ lib/widgets/decorated_box_transition.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyDecoratedBoxTransition extends StatefulWidget {
+  const MyDecoratedBoxTransition({super.key});
+
+  @override
+  State<MyDecoratedBoxTransition> createState() => _MyDecoratedBoxTransitionState();
+}
+
+class _MyDecoratedBoxTransitionState extends State<MyDecoratedBoxTransition> with TickerProviderStateMixin{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
