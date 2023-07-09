6896ef1e42aa5cf13f5335c395cb4fc643c6bab6
KenStarry <starrycodes@gmail.com>
2023-07-09 19:50:35 +0300

    Start decoration

--- lib/widgets/decorated_box_transition.dart
+++ lib/widgets/decorated_box_transition.dart
@@ -9,6 +9,22 @@ class MyDecoratedBoxTransition extends StatefulWidget {
 
 class _MyDecoratedBoxTransitionState extends State<MyDecoratedBoxTransition> with TickerProviderStateMixin{
 
+  final DecorationTween decorationTween = DecorationTween(
+    begin: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(style: BorderStyle.none),
+      borderRadius: BorderRadius.circular(60),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 10,
+          spreadRadius: 3,
+          offset: Offset(0, 6)
+        )
+      ]
+    ),
+    end: BoxDecoration()
+  );
 
   @override
   Widget build(BuildContext context) {
