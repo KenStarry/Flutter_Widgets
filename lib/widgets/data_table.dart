51e789f5db0f97300a94f013b191b3b46e6e467f
KenStarry <starrycodes@gmail.com>
2023-07-09 19:27:36 +0300

    Data table customizations

--- lib/widgets/data_table.dart
+++ lib/widgets/data_table.dart
@@ -6,38 +6,43 @@ class MyDataTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextStyle titles = const TextStyle(fontWeight: FontWeight.bold);
-    return DataTable(columns: [
-      DataColumn(
-          label: Text(
-        'Name',
-        style: titles,
-      )),
-      DataColumn(
-          label: Text(
-        'Age',
-        style: titles,
-      )),
-      DataColumn(
-          label: Text(
-        'Color',
-        style: titles,
-      )),
-    ], rows: [
-      DataRow(cells: [
-        DataCell(Text('Pookie')),
-        DataCell(Text('21')),
-        DataCell(Text('Pink')),
-      ]),
-      DataRow(cells: [
-        DataCell(Text('Starry')),
-        DataCell(Text('22')),
-        DataCell(Text('Blue')),
-      ]),
-      DataRow(cells: [
-        DataCell(Text('Booboo')),
-        DataCell(Text('22')),
-        DataCell(Text('Red')),
-      ]),
-    ]);
+    return DataTable(
+      columns: [
+        DataColumn(
+            label: Text(
+          'Name',
+          style: titles,
+        )),
+        DataColumn(
+            label: Text(
+          'Age',
+          style: titles,
+        )),
+        DataColumn(
+            label: Text(
+          'Color',
+          style: titles,
+        )),
+      ],
+      rows: [
+        DataRow(cells: [
+          DataCell(Text('Pookie')),
+          DataCell(Text('21')),
+          DataCell(Text('Pink')),
+        ], color: MaterialStateProperty.all(Colors.orangeAccent)),
+        DataRow(cells: [
+          DataCell(Text('Starry')),
+          DataCell(Text('22')),
+          DataCell(Text('Blue')),
+        ], color: MaterialStateProperty.all(Colors.orangeAccent)),
+        DataRow(cells: [
+          DataCell(Text('Booboo')),
+          DataCell(Text('22')),
+          DataCell(Text('Red')),
+        ], color: MaterialStateProperty.all(Colors.orangeAccent)),
+      ],
+      columnSpacing: 16,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+    );
   }
 }
